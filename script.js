3fd80aa8e1ac02ae793c0a1624c8ace27314a31a
qiqizhouu02 <yz6897@nyu.edu>
2023-12-03 13:57:41 -0500

    scrolly

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-console.log("Script is running");
+document.addEventListener('scroll', function() {
+    const sections = document.querySelectorAll('.parallax-section');
 
-document.addEventListener("DOMContentLoaded", function() {
-    const parallaxImages = document.querySelectorAll('.parallax-image');
-    const parallaxTexts = document.querySelectorAll('.parallax-text');
+    sections.forEach((section, index) => {
+        const nextSection = sections[index + 1];
+        const rect = section.getBoundingClientRect();
 
-    window.addEventListener('scroll', () => {
-        const scrollY = window.scrollY;
+        // Adding a console.log to see the top value of the current section
+        console.log('Current section top:', rect.top);
 
-        parallaxImages.forEach((image, index) => {
-            const offset = index * 100; // Adjust the offset as needed
-            const opacity = 1 - (scrollY - offset) / 800; // Adjust the divisor as needed
+        if (rect.top < window.innerHeight && rect.bottom >= 0) {
+            // Parallax effect for image
+            section.querySelector('img').style.transform = `translateY(${rect.top * 0.5}px)`;
 
-            image.style.opacity = opacity.toFixed(2);
+            // Text animation
+            if (nextSection) {
+                const nextText = nextSection.querySelector('p');
+                nextText.style.top = `${100 - (rect.bottom / window.innerHeight) * 100}%`;
 
-            // Parallax effect for the image
-            image.style.transform = `translateY(${-(scrollY - offset) * 0.2}px)`;
-        });
-
-        parallaxTexts.forEach((text, index) => {
-            const textOffset = (scrollY - index * 800) * 0.2; // Adjust the multiplier as needed
-            text.style.bottom = `${-100 + textOffset}px`;
-        });
+                // Optional: Log the top value of the next text
+                console.log('Next text top:', nextText.style.top);
+            }
+        }
     });
 });
