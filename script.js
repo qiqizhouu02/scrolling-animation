5f0bc5900d019d5077b43bfe90baf1533de1c643
marjyang <marjorie.y@hotmail.com>
2023-12-03 15:44:52 -0500

    up

--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-document.addEventListener('scroll', function() {
-    const sections = document.querySelectorAll('.parallax-section');
+const sections = document.querySelectorAll('section');
+const imgs = document.querySelectorAll('img');
 
-    sections.forEach((section, index) => {
-        const nextSection = sections[index + 1];
-        const rect = section.getBoundingClientRect();
+window.addEventListener('scroll', () => {
+    let scrollPosition = window.scrollY;
 
-        // Adding a console.log to see the top value of the current section
-        console.log('Current section top:', rect.top);
-
-        if (rect.top < window.innerHeight && rect.bottom >= 0) {
-            // Parallax effect for image
-            section.querySelector('img').style.transform = `translateY(${rect.top * 0.5}px)`;
-
-            // Text animation
-            if (nextSection) {
-                const nextText = nextSection.querySelector('p');
-                nextText.style.top = `${100 - (rect.bottom / window.innerHeight) * 100}%`;
-
-                // Optional: Log the top value of the next text
-                console.log('Next text top:', nextText.style.top);
-            }
+    sections.forEach(section => {
+        if (scrollPosition >= section.offsetTop) {
+            imgs.forEach(img => {
+                img.classList.remove('active');
+                if (section.getAttribute('id') === img.getAttribute('href').substring(1)) {
+                    img.classList.add('active');
+                }
+            });
         }
     });
 });
+
