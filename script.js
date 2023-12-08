a24b3d0a0459f4fd8997a4db7080f186791db128
qiqizhouu02 <yz6897@nyu.edu>
2023-12-08 14:43:38 -0500

    check.visbility

--- script.js
+++ script.js
@@ -1,57 +1,66 @@
 const sections = document.querySelectorAll('section');
 const imgs = document.querySelectorAll('img');
 
-
-window.addEventListener('scroll', () => {
+function checkVisibility() {
     sections.forEach(section => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.offsetHeight;
-        const scrollPosition = window.scrollY;
-        const pulse = document.querySelector('.pulse');
-        const heatOverlay = document.querySelector('.heat-overlay-image');
-        const roofOverlay = document.querySelector('.roof-overlay-image');
-        const rainOverlay = document.querySelector('.rain-overlay');
 
-        const transitionOffset = 100;
+        window.addEventListener('scroll', () => {
+            sections.forEach(section => {
+                const sectionTop = section.offsetTop;
+                const sectionHeight = section.offsetHeight;
+                const scrollPosition = window.scrollY;
+                const pulse = document.querySelector('.pulse');
+                const heatOverlay = document.querySelector('.heat-overlay-image');
+                const roofOverlay = document.querySelector('.roof-overlay-image');
+                const rainOverlay = document.querySelector('.rain-overlay');
 
-        if (scrollPosition + transitionOffset >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
-            imgs.forEach(img => {
-                img.classList.remove('active');
-                if (section.getAttribute('id') === img.getAttribute('href').substring(1)) {
-                    img.classList.add('active');
-                }
-                if (section.getAttribute('id') === 'section1' || section.getAttribute('id') === 'section1'){
-                    rainOverlay.style.opacity = '1';
-                }else{
-                    rainOverlay.style.opacity = '0';
-                }
+                const transitionOffset = 100;
 
-                if (section.getAttribute('id') === 'section3'){
-                    // setTimeout(function() {
-                        heatOverlay.style.opacity = '1';
-                    // }, 1000);
-                }else{
-                    heatOverlay.style.opacity = '0';
-                }
-                if (section.getAttribute('id') === 'section4' || section.getAttribute('id') === 'section5') {
-                    pulse.style.opacity = '1';
-                }else{
-                    pulse.style.opacity = "0";
-                }
-                if (section.getAttribute('id') === 'section7' || section.getAttribute('id') === 'section8'){
-                    // setTimeout(function() {
-                        roofOverlay.style.opacity = '.8';
-                    // }, 2000);
-                }else{
-                    roofOverlay.style.opacity = '0';
-                }
+                if (scrollPosition + transitionOffset >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+                    imgs.forEach(img => {
+                        img.classList.remove('active');
+                        if (section.getAttribute('id') === img.getAttribute('href').substring(1)) {
+                            img.classList.add('active');
+                        }
+                        if (section.getAttribute('id') === 'section1' || section.getAttribute('id') === 'section1') {
+                            rainOverlay.style.opacity = '1';
+                        } else {
+                            rainOverlay.style.opacity = '0';
+                        }
 
-                
+                        if (section.getAttribute('id') === 'section3') {
+                            // setTimeout(function() {
+                            heatOverlay.style.opacity = '1';
+                            // }, 1000);
+                        } else {
+                            heatOverlay.style.opacity = '0';
+                        }
+                        if (section.getAttribute('id') === 'section4' || section.getAttribute('id') === 'section5') {
+                            pulse.style.opacity = '1';
+                        } else {
+                            pulse.style.opacity = "0";
+                        }
+                        if (section.getAttribute('id') === 'section7' || section.getAttribute('id') === 'section8') {
+                            // setTimeout(function() {
+                            roofOverlay.style.opacity = '.8';
+                            // }, 2000);
+                        } else {
+                            roofOverlay.style.opacity = '0';
+                        }
+
+
+                    });
+                }
             });
-        }
+        });
     });
-});
+}
 
+// Event listener for scroll
+window.addEventListener('scroll', checkVisibility);
+
+// Call the function on initial load
+checkVisibility();
 
 // window.addEventListener('scroll', () => {
 //     sections.forEach((section, index) => {
